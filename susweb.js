d21692feb41c088a9ffe404162fc90246d7a973a
Emma <eschalls.mediedesigner@hotmail.com>
2023-05-01 10:58:43 +0200

    indat javascript i en js fil

---
+++ susweb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $(".whatknappe").click(function () {
+        $(".whatindhold").slideToggle("slow");
+    });
+    
+    $(".factsknappe").click(function () {
+        $(".factsindhold").slideToggle("slow");
+    });
+
+    $(".howknappe").click(function () {
+        $(".howindhold").slideToggle("slow");
+    });
+
+    $(".shouldknappe").click(function () {
+        $(".shouldindhold").slideToggle("slow");
+    });
+
+    $(".homeknappe").click(function () {
+        $(".homeindhold").slideToggle("slow");
+    });
+});
